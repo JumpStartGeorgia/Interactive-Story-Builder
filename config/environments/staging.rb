dc9995c5a3b2d29237625f4701972df717a94aa4
jason <jason.addie@gmail.com>
2014-04-30 12:54:03 +0400

    changed subject line

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -62,7 +62,7 @@ BootstrapStarter::Application.configure do
 
 	# options for exception notification gem
 	config.middleware.use ExceptionNotifier,
-		:email_prefix => "[Story Builder App Error (#{Rails.env})] ",
+		:email_prefix => "[Story Builder Dev App Error (#{Rails.env})] ",
 		:sender_address => ENV['APPLICATION_ERROR_FROM_EMAIL'],
 		:exception_recipients => [ENV['APPLICATION_ERROR_TO_EMAIL']]
 end
