512cdca78134e9061e05ffec028a9efc322d6fd8
jason <jason.addie@gmail.com>
2014-05-01 17:26:58 +0400

    fix domain name

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -47,7 +47,7 @@ BootstrapStarter::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.raise_delivery_errors = false
-	config.action_mailer.default_url_options = { :host => 'jumpstart.ge' }
+	config.action_mailer.default_url_options = { :host => 'dev-storybuilder.jumpstart.ge' }
 	config.action_mailer.delivery_method = :smtp
 
   # Enable threaded mode
