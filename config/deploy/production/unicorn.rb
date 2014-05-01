afd7c4b39b04eca9c9c0ad8338123dbf90789697
jason <jason.addie@gmail.com>
2014-05-01 17:37:30 +0400

    increase timeout

--- config/deploy/production/unicorn.rb
+++ config/deploy/production/unicorn.rb
@@ -4,7 +4,7 @@
 root = "/home/story-builder/Story-Builder/current" # path to application current folder
 sock_name = "unicorn_Story-Builder" # must be unique name with no '.'
 port_num = 8126 # must be a unique port number for this application
-tout = 30 # time in seconds before process dies - need a long time for data uploads
+tout = 60 # time in seconds before process dies - need a long time for data uploads
 ##################################
 
 working_directory root
--- config/deploy/staging/unicorn.rb
+++ config/deploy/staging/unicorn.rb
@@ -4,7 +4,7 @@
 root = "/home/story-builder-staging/Story-Builder-Staging/current" # path to application current folder
 sock_name = "unicorn_Story-Builder-Staging" # must be unique name with no '.'
 port_num = 8122 # must be a unique port number for this application
-tout = 30 # time in seconds before process dies - need a long time for data uploads
+tout = 60 # time in seconds before process dies - need a long time for data uploads
 ##################################
 
 working_directory root
