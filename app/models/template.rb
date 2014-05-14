bd73bd497ccbff38b13df6c97745d62e7ed0d910
Antarya <antarya@gmail.com>
2014-05-14 16:23:39 +0400

    CHCA default false

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,6 +1,6 @@
 class Template < ActiveRecord::Base
 	has_many :stories
-	attr_accessible :id,:name,:title,:description,:author,:params,:default
+	attr_accessible :id,:name,:title,:description,:author,:params,:default,:public
 	def self.select_list(self_template_id)
 		where("public = true || id = #{self_template_id}").select("id, title, description").order("title asc")
 	end
