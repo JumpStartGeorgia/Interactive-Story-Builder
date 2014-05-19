4739212da9b23625dfa982dd8e2b76f7eaba6595
antarya <antarya@gmail.com>
2014-05-19 16:52:10 +0400

    Iraklis template

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,11 +37,11 @@ class ApplicationController < ActionController::Base
 	def is_browser_supported?
 		user_agent = UserAgent.parse(request.user_agent)
 logger.debug "////////////////////////// BROWSER = #{user_agent}"
-		if SUPPORTED_BROWSERS.any? { |browser| user_agent < browser }
-			# browser not supported
-logger.debug "////////////////////////// BROWSER NOT SUPPORTED"
-			render "layouts/unsupported_browser", :layout => false
-		end
+# 		if SUPPORTED_BROWSERS.any? { |browser| user_agent < browser }
+# 			# browser not supported
+# logger.debug "////////////////////////// BROWSER NOT SUPPORTED"
+# 			render "layouts/unsupported_browser", :layout => false
+# 		end
 	end
 
 
