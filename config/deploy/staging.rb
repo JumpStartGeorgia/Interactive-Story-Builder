dc9bff4c083b426eb0043b8e65529cdd9acf7717
jason <jason.addie@gmail.com>
2014-06-02 09:29:33 +0400

    change staging deploy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -8,6 +8,6 @@ set :ngnix_conf_file_loc, "staging/nginx.conf" # location of nginx conf file
 set :unicorn_init_file_loc, "staging/unicorn_init.sh" # location of unicor init shell file
 set :github_account_name, "JumpStartGeorgia" # name of accout on git hub
 set :github_repo_name, "Interactive-Story-Builder" # name of git hub repo
-set :git_branch_name, "master" # name of branch to deploy
+set :git_branch_name, "story-settings" # name of branch to deploy
 set :rails_env, "staging" # name of environment: production, staging, ...
 ##################################
