5d11036648965b35e3c4c37f38b4343a03045782
antarya <antarya@gmail.com>
2014-06-04 11:51:15 +0400

    CHCA english template add

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,6 @@
 Template.delete_all
 Template.create(id:1,name:"nytimes",title:"NYTimes Template",description:"Taken from NY Times 'Game of Shark and Minow' story",author:"NY TIMES",default:true)
 Template.create(id:2,name:"chca",title:"CHCA Template",description:"For CHCA specific story - not suggested for use in other stories",author:"Irakli Chumburidze",default:false,public:false)
+Template.create(id:3,name:"chca_en",title:"CHCA Template(en)",description:"For CHCA specific story - not suggested for use in other stories",author:"Irakli Chumburidze",default:false,public:false)
 
 
