6dd027cd4d04f8e1f058e75e794cd14578cfa5f3
antarya <antarya@gmail.com>
2014-06-09 18:48:38 +0400

    storybuilder popup

--- app/assets/javascripts/storyteller.js
+++ app/assets/javascripts/storyteller.js
@@ -64,3 +64,51 @@ $(document).ready(function() {
 
 
 });
+
+
+
+
+$(document).ready(function(){
+  var info = false;
+
+  var currentScroll=0;
+  var infoOffset = 40;
+  $('.story-menu, .story-info-close').click(function()
+  {   
+    
+    if(info)
+    {
+      $('body').css("overflow","visible");
+      $('.story-info-wrapper').fadeOut();
+    }
+        else
+        {
+
+      $('body').css("overflow","hidden");
+      $('.story-info-wrapper').height($(window).height() - 60 - infoOffset);
+    $('.story-info-wrapper').width($(window).width() - infoOffset);
+    $('.story-info-wrapper').css("top",infoOffset/2 + 60);
+    $('.story-info-wrapper').css("left",infoOffset/2);
+      $('.story-info-wrapper').fadeIn();
+    }
+    info=!info;
+
+  });
+
+    $( window ).resize(function() {
+     if(info)
+    {
+        
+        $('.story-info-wrapper').height($(window).height() - 60 - infoOffset);
+        $('.story-info-wrapper').width($(window).width() - infoOffset);
+        $('.story-info-wrapper').css("top",infoOffset/2 + 60);
+        $('.story-info-wrapper').css("left",infoOffset/2);        
+      }
+
+
+    });
+
+
+
+
+  });
