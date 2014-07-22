e7aa6ce861eb1c00b7ece31dd57215bb80e17f93
jason <jason.addie@gmail.com>
2014-07-22 13:15:56 +0400

    helps to test first

--- db/migrate/20140721125453_add_url_shortner_stories.rb
+++ db/migrate/20140721125453_add_url_shortner_stories.rb
@@ -4,8 +4,10 @@ class AddUrlShortnerStories < ActiveRecord::Migration
     
     # create url for existing published stories
     Story.transaction do 
-      Story.is_published.each_with_index do |story, index|
-        puts "processing story #{story.id} | #{story.title} | #{index} of #{story.length} "
+      stories = Story.is_published
+      stories.each_with_index do |story, index|
+        puts "-----"
+        puts "processing story #{story.id} | #{story.title} | #{index} of #{stories.length} "
         story.generate_shortened_url
         story.save
       end
