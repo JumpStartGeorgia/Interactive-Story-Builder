aa2ab53106a983412469b66bc4e1d6878eeaa7d1
jason <jason.addie@gmail.com>
2014-07-29 12:22:08 +0400

    updated move videos migration to work with new prod folder structure

--- db/migrate/20140728085504_move_videos.rb
+++ db/migrate/20140728085504_move_videos.rb
@@ -1,12 +1,14 @@
 class MoveVideos < ActiveRecord::Migration
   def up
-    # move all videos into original sub-folder
+    # move all videos at root of sub-folder into processed folder if already in original folder
+    # - else move into original folder so can be processed
+    # move all images at root of sub-folder into poster folder
     
     path = "#{Rails.root}/public/system/places/video/"
     original = "/original"
     poster = "/poster"
+    processed = "/procesed"
     
-    # for each folder in path, create original folder and move all files into it
     Dir.new(path).each do |dir|
       if !(['.', '..'].include?(dir))
         puts dir
@@ -14,27 +16,47 @@ class MoveVideos < ActiveRecord::Migration
         video_path = "#{path}#{dir}"
         original_path = video_path + original
         poster_path = video_path + poster
+        processed_path = video_path + processed
 
         # create folder if not exist
         FileUtils.mkdir(original_path) if !Dir.exists?(original_path)
         FileUtils.mkdir(poster_path) if !Dir.exists?(poster_path)
+        FileUtils.mkdir(processed_path) if !Dir.exists?(processed_path)
         
         # move files
-        # - movies
-        FileUtils.mv(Dir.glob("#{video_path}/*.mp4"), original_path)
-        # - images
+        # - poster images
         FileUtils.mv(Dir.glob("#{video_path}/*.jpg"), poster_path)
+        # if file is already in original folder
+        # - assume file at root is processed
+        # - do not move file into original
+        Dir.glob("#{video_path}/*.mp4").each do |video|
+          puts "- #{video}"
+          if File.exists?(File.join(original_path, File.basename(video)))
+            puts "-> moving to processed folder"
+            # - move processed video
+            FileUtils.mv(video, processed_path)
+          else
+            puts "-> moving to original folder"
+            # - file is not processed yet
+            # - move original video
+            FileUtils.mv(video, original_path)
+          end
+        end
       end
     end
     
   end
 
   def down
-    # move all videos to root
+    # move all files to root
+    # - leave original folder as is
+    # - move all files in processed to root, 
+    #   then check original folder for any files not processed yet
 
     path = "#{Rails.root}/public/system/places/video/"
     original = "/original"
     poster = "/poster"
+    processed = "/procesed"
 
     # for each folder in path, move files from original folder to root
     Dir.new(path).each do |dir|
@@ -44,17 +66,29 @@ class MoveVideos < ActiveRecord::Migration
         video_path = "#{path}#{dir}"
         original_path = video_path + original
         poster_path = video_path + poster
+        processed_path = video_path + processed
 
         # move files
-        # - movies
-        FileUtils.mv(Dir.glob("#{original_path}/*.mp4"), video_path)
-        # - images
+        # - poster images
         FileUtils.mv(Dir.glob("#{poster_path}/*.jpg"), video_path)
+        # if video is in processed folder, move it to root
+        # else move video from original to root
+        # - move processed movies
+        FileUtils.mv(Dir.glob("#{processed_path}/*.mp4"), video_path)
+        # now check original folder for any files not already at root
+        Dir.glob("#{original_path}/*.mp4").each do |video|
+          puts "- original video: #{video}"
+          if !(File.exists?(video.gsub(original, '')))
+            puts "-> moving to root"
+            FileUtils.mv(video, video_path)
+          end
+        end                
         
         # delete original folder
-        FileUtils.rmdir(original_path)
         FileUtils.rmdir(poster_path)
+        FileUtils.rmdir(processed_path)
       end
     end
   end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140728073320) do
+ActiveRecord::Schema.define(:version => 20140728085504) do
 
   create_table "assets", :force => true do |t|
     t.integer  "item_id"
