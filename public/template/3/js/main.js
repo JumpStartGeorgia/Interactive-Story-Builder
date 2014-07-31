4d3e540480e4c9ef57dabc21fae9f6a8bdd6a36e
antarya <antarya@gmail.com>
2014-07-31 16:18:10 +0400

    chca

---
+++ public/template/3/js/main.js
@@ -0,0 +1,6 @@
+$('#modalAboutProject').on('click', function(e) {        
+     var ml = $(this).attr('data-modalos-id');   
+     var v = $('.navbar-story');      
+     $('#'+ml).modalos({topOffset: $(v).position().top + $(v).height() + 30, width:672, padding:0});
+     e.preventDefault();
+}); 
