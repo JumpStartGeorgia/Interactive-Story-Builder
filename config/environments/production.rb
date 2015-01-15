d21a8aa19cecb482578cbc2874b7502308281496
jason <jason.addie@gmail.com>
2015-01-15 10:34:09 +0400

    changed app name to chai khana

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ BootstrapStarter::Application.configure do
 
 	# options for exception notification gem
 	config.middleware.use ExceptionNotifier,
-		:email_prefix => "[Story Builder App Error (#{Rails.env})] ",
+		:email_prefix => "[Chai Khana App Error (#{Rails.env})] ",
 		:sender_address => ENV['STORY_BUILDER_FROM_EMAIL'],
 		:exception_recipients => [ENV['STORY_BUILDER_ERROR_TO_EMAIL']]
 end
