ae0e812d47e788b5e97ea436d1a3c3547a898ba9
jason <jason.addie@gmail.com>
2015-02-02 11:03:17 +0400

    update item_id in asset to be translation id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150201181227) do
+ActiveRecord::Schema.define(:version => 20150202062339) do
 
   create_table "assets", :force => true do |t|
     t.integer  "item_id"
