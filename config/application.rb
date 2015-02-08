6e766f447c5357e962a92f34cfb895e18e8f3866
jason <jason.addie@gmail.com>
2015-02-08 16:18:43 +0400

    setting fallbacks

--- config/application.rb
+++ config/application.rb
@@ -48,7 +48,8 @@ module BootstrapStarter
     config.i18n.fallbacks = true
 
     # rails will fallback to en, no matter what is set as config.i18n.default_locale
-    config.i18n.fallbacks = [:en]
+    #config.i18n.fallbacks = [:en]
+    config.i18n.fallbacks = {'az' => 'ru', 'hy' => 'ru', 'ka' => 'en', 'ru' => 'en'}
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
