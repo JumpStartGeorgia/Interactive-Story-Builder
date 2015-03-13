f33db98e3681972762295b0ded5568e8350c367c
antarya <antarya@gmail.com>
2015-03-13 17:03:46 +0400

    Export fixed

--- public/media/story/js/page.js
+++ public/media/story/js/page.js
@@ -1,8 +1,24 @@
 $(document).ready(function(){
-   $('#modalAbout').on('click', function(e) {        
-        var ml = $(this).attr('data-modalos-id');   
-        var v = $('.navbar-story');      
-        $('#'+ml).modalos({topOffset: $(v).position().top + $(v).height() + 30, width:672, padding:0});
-        e.preventDefault();
-   }); 
+   $('.section.infographic .container .content.infographic').click(function(){
+     var t = $(this);
+     var image =  $('<img>',
+     {
+         on: 
+         {
+           load: function() { 
+             console.log(this.width);
+             image.modalos({topOffset: 30, leftOffset: 30, width: this.width, height: this.height, paddings:0, margins:0, klass:'infographic', dragscroll:true });
+             
+           },
+           error: function(e) { 
+             console.log(this,' - not loaded'); 
+           }
+         },
+         "src":t.attr('data-original')
+     });
+    
+   });
+   $('.section.infographic .container .content.interactive').click(function(){
+     $($(this).find('.interactive-iframe-popup').html()).modalos({topOffset: 50, leftOffset: 0, fullscreen: true, paddings:0, margins:30, klass:'interactive', dragscroll:true });
+   });
 });
