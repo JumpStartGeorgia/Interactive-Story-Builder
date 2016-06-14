2589b972cbb357911442b571b7543db2b3f62383
Antarya <antarya@gmail.com>
2016-06-14 17:52:00 +0400

    Deploy from meydan

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,6 +8,6 @@ set :ngnix_conf_file_loc, "production/nginx.conf" # location of nginx conf file
 set :unicorn_init_file_loc, "production/unicorn_init.sh" # location of unicor init shell file
 set :github_account_name, "JumpStartGeorgia" # name of accout on git hub
 set :github_repo_name, "Interactive-Story-Builder" # name of git hub repo
-set :git_branch_name, "master" # name of branch to deploy
+set :git_branch_name, "meydan" # name of branch to deploy
 set :rails_env, "production" # name of environment: production, staging, ...
 ##################################
