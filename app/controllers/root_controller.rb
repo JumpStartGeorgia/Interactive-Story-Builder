2673aa52adcbc48d835cf41393b80df301d8fb3d
jason <jason.addie@gmail.com>
2016-07-24 10:35:44 +0400

    bug fix - test for theme before using it

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,68 +1,68 @@
-class RootController < ApplicationController    
-  def index   
-    
+class RootController < ApplicationController
+  def index
+
     @js.push("filter.js")
-    @css.push("navbar.css", "filter.css", "grid.css","root.css")    
+    @css.push("navbar.css", "filter.css", "grid.css","root.css")
     p = (request.xhr? ? params[:page] : 1)
     @stories = process_filter_querystring(Story.is_published.in_published_theme.paginate(:page => p, :per_page => per_page))
     @theme = Theme.for_homepage
-    
 
-    @stories_for_slider = @theme.featured_stories if @theme.present?    
+
+    @stories_for_slider = @theme.featured_stories if @theme.present?
     @navbar_invisible = false
     respond_to do |format|
-      format.html  
-      format.json { render :json => {:d => render_to_string("shared/_grid", :formats => [:html], :layout => false)}}      
+      format.html
+      format.json { render :json => {:d => render_to_string("shared/_grid", :formats => [:html], :layout => false)}}
     end
   end
 
 
-  def author   
+  def author
     @author = Author.find_by_permalink(params[:user_id])
 
     if @author.present?
       @js.push("filter.js","stories.js","follow.js")
       @css.push("navbar.css", "filter.css", "grid.css", "stories.css", "author.css")
-      @stories = process_filter_querystring(Story.by_authors(@author.id).in_published_theme.paginate(:page => params[:page], :per_page => per_page))      
+      @stories = process_filter_querystring(Story.by_authors(@author.id).in_published_theme.paginate(:page => params[:page], :per_page => per_page))
       @editable = (user_signed_in? && current_user.id == @author.id)
-      
+
       @is_following = Notification.already_following_user(current_user.id, @author.id) if user_signed_in?
-      
-      # if(@editable)        
+
+      # if(@editable)
       #   @js.push("modalos.js")
       #   @css.push("modalos.css")
       # end
-      respond_to do |format|     
-        format.html    
-        format.json { render :json => {:d => render_to_string("shared/_grid", :formats => [:html], :layout => false)}}    
-      end    
+      respond_to do |format|
+        format.html
+        format.json { render :json => {:d => render_to_string("shared/_grid", :formats => [:html], :layout => false)}}
+      end
     else
       redirect_to root_path, :notice => t('app.msgs.does_not_exist')
     end
   end
 
-  def embed   
-    #@css.push("embed.css")    
-           
+  def embed
+    #@css.push("embed.css")
+
     @story = Story.is_published.in_published_theme.find_by_permalink(params[:story_id])
-    #redirect_to root_path, :notice => t('app.msgs.does_not_exist')   
-    respond_to do |format|      
-      if @story.present?        
-      
-          if params[:type] == 'full'  
+    #redirect_to root_path, :notice => t('app.msgs.does_not_exist')
+    respond_to do |format|
+      if @story.present?
+
+          if params[:type] == 'full'
                 @is_embed = true
-                @no_nav = true     
+                @no_nav = true
                 @css.push("navbar.css", "navbar2.css", "storyteller.css", "modalos.css")
-                @js.push("storyteller.js","modalos.js", "follow.js")    
+                @js.push("storyteller.js","modalos.js", "follow.js")
                 story = Story.select('stories.id').is_published.in_published_theme.find_by_permalink(params[:id])
-                @story = Story.is_published.in_published_theme.fullsection(story.id) if story.present?  
+                @story = Story.is_published.in_published_theme.fullsection(story.id) if story.present?
                 if @story.present?
-                  # set story locale 
+                  # set story locale
                   # if param exists use that
                   # else check if translation exists for current app locale
                   if params[:sl].present?
-                    @story.current_locale = params[:sl].strip 
-                    Globalize.story_locale = params[:sl].strip 
+                    @story.current_locale = params[:sl].strip
+                    Globalize.story_locale = params[:sl].strip
                   else
                     @story.use_app_locale_if_translation_exists
                   end
@@ -70,30 +70,31 @@ class RootController < ApplicationController
                 # record if the user has liked this story
                 @user_likes = false
                 @user_likes = current_user.voted_up_on? @story if user_signed_in?
-              
-                format.html { render 'storyteller/index', :layout => false }            
+
+                format.html { render 'storyteller/index', :layout => false }
                 impressionist(@story, :unique => [:session_hash])
-          else 
-            format.html { render 'embed', layout: false }    
+          else
+            format.html { render 'embed', layout: false }
           end
-       
+
       else
         format.html { render :file => "#{Rails.root}/public/404", :layout => false, :status => :not_found }
-      end  
-    end 
+      end
+    end
   end
 
   def theme
     @js.push("filter.js")
-    @css.push("navbar.css", "filter.css", "grid.css","root.css")    
+    @css.push("navbar.css", "filter.css", "grid.css","root.css")
     @theme = Theme.published.find_by_permalink(params[:id])
-    @stories = process_filter_querystring(Story.is_published.in_published_theme.by_theme(@theme.id).paginate(:page => params[:page], :per_page => per_page))      
-    @stories_for_slider = @theme.featured_stories if @theme.present?
 
     if @theme.present?
+      @stories = process_filter_querystring(Story.is_published.in_published_theme.by_theme(@theme.id).paginate(:page => params[:page], :per_page => per_page))
+      @stories_for_slider = @theme.featured_stories
+
       respond_to do |format|
-        format.html  
-        format.json { render :json => {:d => render_to_string("shared/_grid", :formats => [:html], :layout => false)}}      
+        format.html
+        format.json { render :json => {:d => render_to_string("shared/_grid", :formats => [:html], :layout => false)}}
       end
     else
       redirect_to root_path, :notice => t('app.msgs.does_not_exist')
@@ -105,21 +106,21 @@ class RootController < ApplicationController
 
     if @story_type.present?
       respond_to do |format|
-        format.html  
-        #format.json { render json: @stories }      
-        format.json { render :json => {:d => render_to_string("shared/_grid", :formats => [:html], :layout => false)}}      
+        format.html
+        #format.json { render json: @stories }
+        format.json { render :json => {:d => render_to_string("shared/_grid", :formats => [:html], :layout => false)}}
       end
     else
       redirect_to root_path, :notice => t('app.msgs.does_not_exist')
     end
   end
-  
+
   def feedback
-    @css.push("navbar.css")   
+    @css.push("navbar.css")
     @message = Message.new(:mailer_type => Message::MAILER_TYPE[:feedback])
 
     @message_types = []
-    Message::TYPE.each{|k,v| @message_types << [I18n.t("message_types.#{k}"), v]} 
+    Message::TYPE.each{|k,v| @message_types << [I18n.t("message_types.#{k}"), v]}
     @message_types.sort_by!{|x| x[0]}
 
     if request.post?
@@ -131,19 +132,19 @@ class RootController < ApplicationController
 		  end
 	  end
     respond_to do |format|
-      format.html 
+      format.html
     end
   end
 
 
-  def about   
-    @css.push("navbar.css")   
+  def about
+    @css.push("navbar.css")
 
     @page = Page.by_name('about')
-  
+
     if @page.present?
       respond_to do |format|
-        format.html 
+        format.html
       end
     else
 		  flash[:info] =  t('app.msgs.does_not_exist')
@@ -158,21 +159,21 @@ class RootController < ApplicationController
     @filtered_by_category = ""
     if index.present?
       @filtered_by_category = @categories_published[index].permalink
-      @items = @items.by_category(@categories_published[index].id)    
+      @items = @items.by_category(@categories_published[index].id)
     end
    @filtered_by_tag = ""
    if params[:tag].present?
       @filtered_by_tag = params[:tag]
-      @items =  @items.tagged_with(params[:tag])                
-    end    
+      @items =  @items.tagged_with(params[:tag])
+    end
     @filter_by = ""
     @filter_by = ("category = " + @filtered_by_category + (@filtered_by_tag.present? ? ", " : "")) if @filtered_by_category.present?
-    @filter_by += "tag = " + @filtered_by_tag  if @filtered_by_tag.present?   
+    @filter_by += "tag = " + @filtered_by_tag  if @filtered_by_tag.present?
     @filter_by = "nothing" if @filter_by.empty?
 
     respond_to do |format|
       format.atom { render :layout => false }
     end
   end
-  
+
 end
