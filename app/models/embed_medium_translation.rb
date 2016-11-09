441f0f73b3e3ca182c3af199bf43496e501e524f
Antarya <antarya@gmail.com>
2016-11-09 20:03:24 +0400

    Embed media extending functionality

--- app/models/embed_medium_translation.rb
+++ app/models/embed_medium_translation.rb
@@ -7,8 +7,8 @@ class EmbedMediumTranslation < ActiveRecord::Base
 
   #################################
   ## Validations
-  validates :title, :presence => true, length: { maximum: 255}  
-  validates :url, :code, :presence => true
+  validates :title, :presence => true, length: { maximum: 255}
+  validates :code, :presence => true
   validates :url, :format => {:with => URI::regexp(['http','https']), :message => I18n.t('errors.messages.invalid_format_url') }, :if => "!url.blank?"
   validates_uniqueness_of :embed_medium_id, scope: [:locale]
   #################################
@@ -16,4 +16,5 @@ class EmbedMediumTranslation < ActiveRecord::Base
   amoeba do
     enable
   end
+
 end
