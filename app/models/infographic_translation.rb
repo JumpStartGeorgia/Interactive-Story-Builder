048a08348606891cd09aea6184e58171443f6e80
Antarya <antarya@gmail.com>
2016-11-21 11:45:29 +0400

    Infographic url when invalid catch exception

--- app/models/infographic_translation.rb
+++ app/models/infographic_translation.rb
@@ -1,14 +1,14 @@
 class InfographicTranslation < ActiveRecord::Base
   belongs_to :infographic
 
-  has_one :image,     
-    :conditions => "asset_type = #{Asset::TYPE[:infographic]}",    
+  has_one :image,
+    :conditions => "asset_type = #{Asset::TYPE[:infographic]}",
     foreign_key: :item_id,
     class_name: "Asset",
     dependent: :destroy
 
-  has_one :dataset_file,     
-    :conditions => "asset_type = #{Asset::TYPE[:infographic_dataset]}",    
+  has_one :dataset_file,
+    :conditions => "asset_type = #{Asset::TYPE[:infographic_dataset]}",
     foreign_key: :item_id,
     class_name: "Asset",
     dependent: :destroy
@@ -19,44 +19,52 @@ class InfographicTranslation < ActiveRecord::Base
   accepts_nested_attributes_for :image, :reject_if => lambda { |c| c[:asset].blank? && c[:asset_clone_id].blank? }
   accepts_nested_attributes_for :dataset_file, :reject_if => lambda { |c| c[:asset].blank? && c[:asset_clone_id].blank? }
 
-  attr_accessible :infographic_id, :locale, :title, :caption, :description, :dataset_url, 
-                  :image_attributes, :dataset_file_attributes, :infographic_datasources_attributes, 
+  attr_accessible :infographic_id, :locale, :title, :caption, :description, :dataset_url,
+                  :image_attributes, :dataset_file_attributes, :infographic_datasources_attributes,
                   :subtype, :dynamic_url, :dynamic_code, :width, :height
 
   attr_accessor :is_progress_increment, :progress_story_id, :width, :height
 
   #################################
   ## Validations
-  validates :title, :presence => true, length: { maximum: 255}    
-  # validates :image, presence: true, if: :static_type? 
+  validates :title, :presence => true, length: { maximum: 255}
+  # validates :image, presence: true, if: :static_type?
   validates :dataset_url, :format => {:with => URI::regexp(['http','https']), :message => I18n.t('errors.messages.invalid_format_url') }, :if => "!dataset_url.blank?"
-  validates :dynamic_url, presence: true, :format => {:with => URI::regexp(['http','https']), :message => I18n.t('errors.messages.invalid_format_url') }, if: :dynamic_type? 
+  validates :dynamic_url, presence: true, :format => {:with => URI::regexp(['http','https']), :message => I18n.t('errors.messages.invalid_format_url') }, if: :dynamic_type?
   validate :generate_iframe
   validate :check_asset_existence
   validates_uniqueness_of :infographic_id, scope: [:locale]
-  def check_asset_existence   
+  def check_asset_existence
     if static_type? && self.image.blank?
       errors.add(:image_name, I18n.t('activerecord.errors.messages.not_provided'))
     end
   end
 
   def generate_iframe
-    if dynamic_type? 
+    if dynamic_type?
       ok = false
       html = ''
       u = self.dynamic_url
       if u.present?
-
-        uri = URI.parse(u)
-        html =  '<iframe '+(self.width.present? ? ' width="'+self.width.to_s+'"': '')+(self.height.present? ? ' height ="'+self.height.to_s+'"': '') + ' src="'+self.dynamic_url+'" frameborder="0" allowfullscreen class="infographic' + (self.height.blank? ? ' height': '') +'" sandbox="allow-scripts allow-same-origin"></iframe>' 
+        begin
+          uri = URI.parse(u)
+          if !(uri.kind_of?(URI::HTTP) || uri.kind_of?(URI::HTTPS))
+            self.errors.add(:dynamic_url, I18n.t('stories.youtube.generate_iframe.error'))
+            return false
+          end
+        rescue
+          self.errors.add(:dynamic_url, I18n.t('stories.youtube.generate_iframe.error'))
+          return false
+        end
+        html =  '<iframe '+(self.width.present? ? ' width="'+self.width.to_s+'"': '')+(self.height.present? ? ' height ="'+self.height.to_s+'"': '') + ' src="'+self.dynamic_url+'" frameborder="0" allowfullscreen class="infographic' + (self.height.blank? ? ' height': '') +'" sandbox="allow-scripts allow-same-origin"></iframe>'
         ok = true
-      end   
+      end
 
-      if ok       
+      if ok
         self.dynamic_code = html
         return true
       else
-       self.errors.add(:dynamic_code, I18n.t('stories.youtube.generate_iframe.error'))       
+       self.errors.add(:dynamic_code, I18n.t('stories.youtube.generate_iframe.error'))
        return false
       end
     else
@@ -75,17 +83,17 @@ class InfographicTranslation < ActiveRecord::Base
 
   def image_exists?
     self.image.present? && self.image.file.exists?
-  end  
+  end
 
   def dataset_file_exists?
     self.dataset_file.present? && self.dataset_file.file.exists?
-  end  
+  end
 
 private
-  def static_type?    
+  def static_type?
     self.subtype == Infographic::TYPE[:static]
   end
-  def dynamic_type?    
+  def dynamic_type?
     self.subtype == Infographic::TYPE[:dynamic]
   end
 
