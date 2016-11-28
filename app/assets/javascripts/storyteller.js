c90439412c7c26e96896e200085d559e4a9d353e
Antarya <antarya@gmail.com>
2016-11-28 15:27:09 +0400

    mac scroll issue test

--- app/assets/javascripts/storyteller.js
+++ app/assets/javascripts/storyteller.js
@@ -6,41 +6,44 @@ function story_new_comment_callback(url){
   // ({
   //   url: url + '/record_comment',
   //   dataType: 'json'
-  // }).done(function(d) { 
+  // }).done(function(d) {
   //   // update the comment count on the page
   //   $('#comments-count').html(d.count);
   // });
 }
+
+
 $(document).ready(function(){
+    var is_mac = navigator.platform.indexOf('Mac') > -1;
     if($(".navbar-storybuilder").length != 0)
     {
       //$(".navbar-story").css("top","54px");
       var lastScroll = 0;
       var sn = true;
       var sbn = true;
-      $(window).scroll(function(event){
-          //Sets the current scroll position
-          var st = $(this).scrollTop();
-          var sbh = $(".navbar-storybuilder").height();
-          //Determines up-or-down scrolling
+
+      addWheelListener(document, function (event) {
+        if(event.deltaY !== -0 && event.deltaY !== 0) {
+          if(is_mac && Math.abs(event.deltaY) < 10) { return; }
+
+          var st = $(this).scrollTop(),
+            sbh = $(".navbar-storybuilder").height();
+
           if (st > lastScroll){ //down
-             if(sbn)
-             {
-                $(".navbar-storybuilder").animate({top:-1*sbh},300);
-                //$(".navbar-story").animate({top:'0px'}, 300);
-            sbn = false;
+            if(sbn) {
+              $(".navbar-storybuilder").animate({top:-1*sbh},300);
+              sbn = false;
             }
           }
-          else {//up             
-                  if(!sbn)
-                  {                    
-                    $(".navbar-storybuilder").animate({top:'0px'},300);
-                    //$(".navbar-story").animate({top:sbh}, 300);
-                    sbn = true;                
-                  }
+          else { //up
+            if(!sbn) {
+              $(".navbar-storybuilder").animate({top:'0px'},300);
+              sbn = true;
+            }
           }
           //Updates scroll position
           lastScroll = st;
+        }
       });
     }
 
@@ -49,21 +52,21 @@ $(document).ready(function(){
         //var ml =  $('<div id="modalos-infographic"></div>');
         var image =  $('<img>',
         {
-            on: 
+            on:
             {
-              load: function() { 
+              load: function() {
                 console.log(this.width);
-                 var v = $('.navbar-storybuilder');   
+                 var v = $('.navbar-storybuilder');
                 image.modalos({topOffset: v.position().top + v.height() + 30, leftOffset: 30, width: this.width, height: this.height, paddings:0, margins:0, klass:'infographic', dragscroll:true });
-                
+
               },
-              error: function(e) { 
-                console.log(this,' - not loaded'); 
+              error: function(e) {
+                console.log(this,' - not loaded');
               }
             },
             "src":t.attr('data-original')
         });
-       
+
       });
 
 
@@ -72,34 +75,101 @@ $(document).ready(function(){
       });
 
 
-      $('.modalEmbed').on('click', function(e) {      
-        var v = $('.navbar-storybuilder');   
+      $('.modalEmbed').on('click', function(e) {
+        var v = $('.navbar-storybuilder');
         $($('#modalos-embed').get(0).outerHTML).modalos({topOffset: v.position().top + v.height() + 30, width:360, klass:'embed'}); // height:370,
         e.preventDefault();
-      });     
+      });
 
-      $(document).on('click','.embed-type-switcher > div',function(){        
-        $('.embed-type-switcher > div').each(function(){$(this).toggleClass('selected');}); 
+      $(document).on('click','.embed-type-switcher > div',function(){
+        $('.embed-type-switcher > div').each(function(){$(this).toggleClass('selected');});
         var frame_code = $('.embed-code textarea');
         var iframe_width= $(this).data('width');
         var embed_type = 'partial';
-        if($(this).hasClass('full')) 
-        {                    
+        if($(this).hasClass('full'))
+        {
           embed_type = 'full';
-        }        
+        }
         frame_code.text("<iframe src='" +  frame_code.attr('data-iframe-link') + "?type="+embed_type+"' width='"+iframe_width+"' height='100%' frameborder='0'></iframe>");
         $('.iframe-size').val(iframe_width);
       });
 
       $(document).on('change', '.iframe-size', function(){
         var iframe_width=$(this).val();
-        var frame_code = $('.embed-code textarea'); 
+        var frame_code = $('.embed-code textarea');
         var embed_type = 'partial';
-        
-        if($('.embed-type-switcher > div.selected').hasClass('full')) 
-        {                    
+
+        if($('.embed-type-switcher > div.selected').hasClass('full'))
+        {
           embed_type = 'full';
-        }        
+        }
         frame_code.text("<iframe src='" +  frame_code.attr('data-iframe-link') + "?type="+embed_type+"' width='"+iframe_width+"' height='100%' frameborder='0'></iframe>");
       });
   });
+
+
+// creates a global "addWheelListener" method
+// example: addWheelListener( elem, function( e ) { console.log( e.deltaY ); e.preventDefault(); } );
+(function(window,document) {
+
+    var prefix = "", _addEventListener, support;
+
+    // detect event model
+    if ( window.addEventListener ) {
+        _addEventListener = "addEventListener";
+    } else {
+        _addEventListener = "attachEvent";
+        prefix = "on";
+    }
+
+    // detect available wheel event
+    support = "onwheel" in document.createElement("div") ? "wheel" : // Modern browsers support "wheel"
+              document.onmousewheel !== undefined ? "mousewheel" : // Webkit and IE support at least "mousewheel"
+              "DOMMouseScroll"; // let's assume that remaining browsers are older Firefox
+
+    window.addWheelListener = function( elem, callback, useCapture ) {
+        _addWheelListener( elem, support, callback, useCapture );
+
+        // handle MozMousePixelScroll in older Firefox
+        if( support == "DOMMouseScroll" ) {
+            _addWheelListener( elem, "MozMousePixelScroll", callback, useCapture );
+        }
+    };
+
+    function _addWheelListener( elem, eventName, callback, useCapture ) {
+        elem[ _addEventListener ]( prefix + eventName, support == "wheel" ? callback : function( originalEvent ) {
+            !originalEvent && ( originalEvent = window.event );
+
+            // create a normalized event object
+            var event = {
+                // keep a ref to the original event object
+                originalEvent: originalEvent,
+                target: originalEvent.target || originalEvent.srcElement,
+                type: "wheel",
+                deltaMode: originalEvent.type == "MozMousePixelScroll" ? 0 : 1,
+                deltaX: 0,
+                deltaY: 0,
+                deltaZ: 0,
+                preventDefault: function() {
+                    originalEvent.preventDefault ?
+                        originalEvent.preventDefault() :
+                        originalEvent.returnValue = false;
+                }
+            };
+
+            // calculate deltaY (and deltaX) according to the event
+            if ( support == "mousewheel" ) {
+                event.deltaY = - 1/40 * originalEvent.wheelDelta;
+                // Webkit also support wheelDeltaX
+                originalEvent.wheelDeltaX && ( event.deltaX = - 1/40 * originalEvent.wheelDeltaX );
+            } else {
+                event.deltaY = originalEvent.detail;
+            }
+
+            // it's time to fire the callback
+            return callback( event );
+
+        }, useCapture || false );
+    }
+
+})(window,document);
