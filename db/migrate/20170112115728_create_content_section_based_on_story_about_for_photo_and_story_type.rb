86298c8c92bd907d72fc0ff56a2c0be6451d1e7d
Antarya <antarya@gmail.com>
2017-01-25 14:35:38 +0400

    Migration script updated to use all sections, empty stories ignored, only published story processed

--- db/migrate/20170112115728_create_content_section_based_on_story_about_for_photo_and_story_type.rb
+++ db/migrate/20170112115728_create_content_section_based_on_story_about_for_photo_and_story_type.rb
@@ -2,80 +2,111 @@ class CreateContentSectionBasedOnStoryAboutForPhotoAndStoryType < ActiveRecord::
   def up
     require "./lib/text.rb"
     white = TextSimilarity::WhiteSimilarity.new
-    stories = Story.where(id: 381)#.where(story_type_id: [2,3,4,5])#
+    stories = Story.where(story_type_id: [2,3,4,5])#.where(id: 381)
     puts "Stories to process: #{stories.length}"
     stories.each { |story|
       I18n.locale = story.story_locale
       story.current_locale = story.story_locale
       story_about_orig = ActionController::Base.helpers.sanitize ActionController::Base.helpers.strip_tags story.about
-      puts "  #{story.title}"
-      if !story_about_orig.present?
+      puts "#{story.id} - #{story.title}"
+      if !story.published || !story_about_orig.present?
         puts "--------- original about is missing"
         next
       end
       # story_about_orig = story_about_orig
-      secs = story.sections.select{|t| t.ok? }.sort_by{ |p| p.position }
+      secs = story.sections.sort_by{ |p| p.position } #.select{|t| t.ok? }
       similar_section = nil
       first_section = secs.first
-      secs.each { |sec|
-        if sec.content?
-          section_text = ActionController::Base.helpers.sanitize ActionController::Base.helpers.strip_tags sec.content.text
-          ratio = white.similarity(story_about_orig, section_text)
-          similar_section = sec if ratio > 0.90
-        end
-      }
-      if similar_section.nil?
-        if !first_section.infographic?
-          new_section = Section.create(story_id: story.id, type_id: 1, has_marker: 0, title: "story_about_text" )
-
-          if first_section.media?
-            puts "--------- moving as second"
-            new_section.insert_at(2)
-          else
-            puts "--------- moving as first"
-            new_section.move_to_top
+      if first_section.present?
+        secs.each { |sec|
+          if sec.content?
+            section_text = ActionController::Base.helpers.sanitize ActionController::Base.helpers.strip_tags sec.content.text
+            ratio = white.similarity(story_about_orig, section_text)
+            similar_section = sec if ratio > 0.90
           end
+        }
+        if similar_section.nil?
+          if !first_section.infographic?
+            new_section = Section.create(story_id: story.id, type_id: 1, has_marker: 0, title: "story_about_text" )
 
-          new_content = Content.create(:section_id => new_section.id, title: "about", text: ("<p>" + story_about_orig + "</p>"))
-          pl = story.published_locales
-          pl.each {|l|
-            next if l == story.story_locale
-            ls = l.to_sym
-            story.current_locale = ls
-            new_content.current_locale = ls
-            new_section.current_locale = ls
-            story_about = "<p>" + (story.about.present? ?  (ActionController::Base.helpers.sanitize ActionController::Base.helpers.strip_tags story.about) : story_about_orig)  + "</p>"
-            new_section.update_attributes({title: "story_about_text"})
-            new_content.update_attributes({title: "about", text: story_about })
-          }
+            if first_section.media?
+              puts "--------- moving as second"
+              new_section.insert_at(2)
+            else
+              puts "--------- moving as first"
+              new_section.move_to_top
+            end
+
+            new_content = Content.create(:section_id => new_section.id, title: "about", text: ("<p>" + story_about_orig + "</p>"))
+            pl = story.story_locales
+            pl.each {|l|
+              next if l == story.story_locale
+              ls = l.to_sym
+              story.current_locale = ls
+              new_content.current_locale = ls
+              new_section.current_locale = ls
+              if story.about.present?
+                story_about = "<p>" + (ActionController::Base.helpers.sanitize ActionController::Base.helpers.strip_tags story.about) + "</p>"
+                new_section.update_attributes({title: "story_about_text"})
+                new_content.update_attributes({title: "about", text: story_about })
+              end
+            }
+          else
+            puts "--------- infographic"
+            pl = story.story_locales
+            pl.each {|l|
+              ls = l.to_sym
+              story.current_locale = ls
+              first_section.current_locale = ls
+              if story.about.present?
+                cont = first_section.infographic
+                cont.current_locale = ls
+                cont.update_attributes({description: "<p>" + (ActionController::Base.helpers.sanitize ActionController::Base.helpers.strip_tags story.about)  + "</p>" + cont.description })
+              end
+            }
+          end
         else
-          puts "--------- infographic"
-          pl = story.story_locales
-          pl.each {|l|
-            ls = l.to_sym
-            story.current_locale = ls
-            first_section.current_locale = ls
-            cont = first_section.infographic
-            cont.current_locale = ls
-            cont.update_attributes({description: "<p>" + (story.about.present? ?  (ActionController::Base.helpers.sanitize ActionController::Base.helpers.strip_tags story.about) : story_about_orig)  + "</p>" + cont.description })
-          }
+          puts "--------- already existent content with about text"
         end
-      else
-        puts "--------- already existent content with about text"
       end
     }
   end
 
   def down
-    stories = Story.where(id: 381)#.where(story_type_id: [2,3,4,5])#.where(id: 353)
+    stories = Story.where(story_type_id: [2,3,4,5])#.where(id: 381)
     puts "Stories to process: #{stories.length}"
     stories.each {|story|
+      I18n.locale = story.story_locale
+      story.current_locale = story.story_locale
+
       puts "  #{story.title}"
       secs = story.sections.select{|t| t.content? && t.title == "story_about_text" && t.content.title == "about" }.first
       if secs.present?
         secs.destroy
+        puts "--------- had story about text as section"
       else
-        puts "--------- already without about text content"
+
+        first_section = story.sections.sort_by{ |p| p.position }.first #.select{|t| t.ok? }
+        if first_section.present? && first_section.infographic?
+          pl = story.story_locales
+          pl.each {|l|
+            ls = l.to_sym
+            story.current_locale = ls
+            first_section.current_locale = ls
+            if story.about.present?
+              story_about_text = ActionController::Base.helpers.sanitize ActionController::Base.helpers.strip_tags story.about
+              cont = first_section.infographic
+              cont.current_locale = ls
+              section_text = cont.description
+              if section_text.index("<p>"+story_about_text+"</p>").present?
+                cont.update_attributes({ description: section_text.gsub("<p>"+story_about_text+"</p>", "") })
+                puts "--------- infographic description had story about"
+              end
+            end
+          }
+        else
+          puts "--------- already without about text content"
+        end
       end
     }
   end
