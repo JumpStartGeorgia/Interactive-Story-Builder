c555c945041b2e383df05f747491363cc23d2c4a
Antarya <antarya@gmail.com>
2017-02-07 19:27:19 +0400

    Translation fixes

--- db/migrate/20170207065931_update_translations.rb
+++ db/migrate/20170207065931_update_translations.rb
@@ -3,7 +3,7 @@ class UpdateTranslations < ActiveRecord::Migration
     StoryTypeTranslation.where({story_type_id: 5, locale: 'ru'}).update_all({name: "Инфографика"})
     StoryTypeTranslation.where({story_type_id: 5, locale: 'hy'}).update_all({name: "ինֆոգրաֆիկա"})
     StoryTypeTranslation.where({story_type_id: 5, locale: 'ka'}).update_all({name: "ინფოგრაფიკა"})
-    StoryTypeTranslation.where({story_type_id: 5, locale: 'az'}).update_all({name: "infografik"})
+    StoryTypeTranslation.where({story_type_id: 5, locale: 'az'}).update_all({name: "infografika"})
     StoryTypeTranslation.where({story_type_id: 2, locale: 'az'}).update_all({name: "tok şou"})
     Language.where({locale: 'az'}).update_all({name: "Azərbaycan"})
   end
