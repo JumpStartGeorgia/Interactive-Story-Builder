f3023af46e9652770f4bd42bdb44674ead97ebdc
Antarya <antarya@gmail.com>
2017-02-07 12:53:08 +0400

    Az translation updated

--- db/migrate/20170207065931_update_translations.rb
+++ db/migrate/20170207065931_update_translations.rb
@@ -5,6 +5,7 @@ class UpdateTranslations < ActiveRecord::Migration
     StoryTypeTranslation.where({story_type_id: 5, locale: 'ka'}).update_all({name: "ინფოგრაფიკა"})
     StoryTypeTranslation.where({story_type_id: 5, locale: 'az'}).update_all({name: "infografik"})
     StoryTypeTranslation.where({story_type_id: 2, locale: 'az'}).update_all({name: "tok şou"})
+    Language.where({locale: 'az'}).update_all({name: "Azərbaycan"})
   end
 
   def down
